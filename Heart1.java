9f01b65054caf381f98344628b802ad56857b562
latifhaqjou <haqjoul@gmail.com>
2024-11-05 11:09:23 +0430

    Add files via upload

---
+++ Heart1.java
@@ -0,0 +1,10 @@
+package heaart;
+
+public class Heart1 {
+public static void main(String[] args) {
+     for(int  num1=1;num1<=12;num1++)
+     {for(int num2=1;num2<=12;num2++)
+        {System.out.print(num1*num2+"\t");}}
+System.out.println();
+}
+}
